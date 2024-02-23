53a87424b407d19c37c28458be38523288de3530
Siddhant Vishnu <siddhant.m.vishnu@gmail.com>
2024-02-23 13:28:34 +0530

    Database Schema Created

--- backend/database/db.js
+++ backend/database/db.js
@@ -5,24 +5,79 @@ const url = "mongodb+srv://siddhantmvishnu:PBLproject@messmanagement.ii1mv9k.mon
 mongoose.connect(url);
 
 const userSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    mobile: Number,
-    email: String,
-    password: String
+    data: {
+        firstname: String,
+        lastname: String,
+        mobile: Number,
+        email: String,
+        password: String
+    },
+    myCart: {
+        fullThali: Number,
+        halfThali1: Number,
+        halfThali2: Number,
+        halfThali3: Number
+    },
+    myOrders: {
+        fullThali: Number,
+        halfThali1: Number,
+        halfThali2: Number,
+        halfThali3: Number
+    }
 })
 
 const adminSchema = new mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    mobile: Number,
-    email: String,
-    password: String
+    data: {
+        firstname: String,
+        lastname: String,
+        mobile: Number,
+        email: String,
+        password: String
+    },
+})
+
+const menuSchema = new mongoose.Schema({
+    fullThali: {
+        dryVeg: String,
+        gravyVeg: String,
+        rice: String,
+        dal: String,
+        chapati: String,
+        salad: String
+    },
+    halfThali1: {
+        dryVeg: String,
+        chapati: String,
+        salad: String
+    },
+    halfThali2: {
+        gravyVeg: String,
+        chapati: String,
+        salad: String
+    },
+    halfThali3: { 
+        rice: String,
+        dal: String,
+        salad: String
+    }
+})
+
+const pendingOrderSchema = new mongoose.Schema({
+    username: String,
+    otp: Number,
+    order: {
+        fullThali: Number,
+        halfThali1: Number,
+        halfThali2: Number,
+        halfThali3: Number
+    }
 })
 
 const USER = mongoose.model('User',userSchema);
 const ADMIN = mongoose.model('Admin', adminSchema);
+const MENU = mongoose.model('Menu', menuSchema);
+const PENDING_ORDERS = mongoose.model('Pending-Orders', pendingOrderSchema);
 
 module.exports = {
-    USER, ADMIN
+    USER, ADMIN, MENU, PENDING_ORDERS
 }
--- backend/endpoints/index.js
+++ backend/endpoints/index.js
@@ -10,12 +10,23 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', function(req, res) {
-
+    
 });
 
+// Main user landing page endpoints
 app.get('/user', function(req, res) {
-
+    // 1. Main Menu db call
 });
+
+// My-Cart page for user
+app.get('/user/my-cart', function(req, res) {
+    // 1. My Cart db call
+});
+
+//My-orders page for user
+app.get('/user/my-orders', function(req, res) {
+    // 1. My order db call 
+})
 
 app.get('/admin', function(req, res) {
 
